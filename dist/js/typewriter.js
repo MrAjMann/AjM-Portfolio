53627bac918d3261198c563a794ad30477034ef7
MrAjMann <anthonyjmann87@gmail.com>
2020-07-24 15:27:58 +1000

    Updated Projects and About page, also removed the blog page as I have not content for this section

--- dist/js/typewriter.js
+++ dist/js/typewriter.js
@@ -28,7 +28,7 @@ class TypeWriter {
     this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
 
     // Initial Type Speed
-    let typeSpeed = 100;
+    let typeSpeed = 300;
 
     if (this.isDeleting) {
       typeSpeed /= 2;
